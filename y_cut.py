7e2ee63ff4017a813d0751874811e12589137897
-水上- <yuki@minakami.cn>
2019-09-02 16:13:55 +0800

    9/2 下午

--- y_cut.py
+++ y_cut.py
@@ -5,30 +5,6 @@ import numpy as np
 import csv
 
 
-# x = ['10.0', '30.0', '50.0', '70.0', '90.0', '110.0', '130.0',
-# '150.0', '170.0', '190.0', '210.0', '230.0', '250.0', '270.0',
-# '290.0', '310.0', '330.0', '350.0', '370.0', '390.0', '410.0',
-# '430.0', '450.0', '470.0', '490.0']
-
-# y = ['10.0', '30.0', '50.0', '70.0', '90.0', '110.0', '130.0',
-# '150.0', '170.0', '190.0', '210.0', '230.0', '250.0', '270.0',
-# '290.0', '310.0', '330.0', '350.0', '370.0', '390.0', '410.0',
-# '430.0', '450.0', '470.0', '490.0']
-
-# z = ['-2.0', '-6.0', '-10.0', '-14.0', '-18.0', '-22.0', '-26.0',
-# '-30.0', '-34.0', '-38.0', '-42.0', '-46.0', '-50.0', '-54.0', '-58.0']
-
-# 在这里获取x的所有取值
-# x = []
-# with open('Plot_coord.csv', 'r') as zb:
-#     reader = csv.reader(zb)
-#     colx = [row[2] for row in reader]
-#     # x=[]
-#     for element in colx:
-#         if (element not in x):
-#             x.append(element)
-#     print(x)
-
 def y_cut(time,attri,y_index,grid_line,x,y,z):
     print("y_cut得到的时间文件是：" + time)
     print(attri)
@@ -68,11 +44,14 @@ def y_cut(time,attri,y_index,grid_line,x,y,z):
     print("run at line 67")
     #这里的坐标很混乱（但是能用），等一个有缘人来改一改
     #固定了y的坐标，reshape（x，z）
+    # 9.2 这里试图直接通过坐标来旋转图像
     Z = np.array(col2).reshape(x, z)
+    # 9.2 上面这一行绝对不能动
     #现在的x对应z  &&&&&&&  y对应x
-    xx = np.arange(0, z)
-    yy = np.arange(0, x)
-    X, Y = np.meshgrid(xx, yy)
+    xx = np.arange(0, x)
+    yy = np.arange(0, z)
+    Y, X = np.meshgrid(yy, xx)
+    # 9.2 改了上面这一行，似乎可行
 
     g = plt.contourf(X, Y, Z, len(colors), alpha=0.8, cmap=plt.cm.jet)
     #plt.clabel(g, inline=1, fontsize=10)
@@ -82,6 +61,7 @@ def y_cut(time,attri,y_index,grid_line,x,y,z):
         plt.grid(color='black')
     elif grid_line == 0:
         pass
+
     plt.show()
 
 #y_cut(" 1.55000000E+03.csv",1,"10",1,25,25,15)
