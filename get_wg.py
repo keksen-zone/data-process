65665996c31bf6892ee5cd37bf9f82aaa7fed403
-水上- <yuki@minakami.cn>
2019-09-02 15:51:29 +0800

    9/2 下午

--- get_wg.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def get_wg():
-    with open('wg.txt','r') as f:
-        line = f.readline()
-        x = line.split(' ')[0]
-        y = line.split(' ')[1]
-        z = line.split(' ')[2]
-        print(x+" "+y+" "+z)
-        return int(x), int(y), int(z)
